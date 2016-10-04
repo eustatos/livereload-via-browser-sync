d6d77a6db5b39b3fed92cd3d913e7ba1a6b9a876
eustatos <astashkinav@gmail.com>
2016-10-04 21:13:51 +0400

    livereload via browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,45 @@
-var lr = require('tiny-lr');
 var gulp = require('gulp');
-var livereload = require('gulp-livereload');
 var imagemin = require('gulp-imagemin');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var connect = require('connect');
-var server = lr();
 var less = require('gulp-less');
 var LessAutoprefix = require('less-plugin-autoprefix');
 var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+var browserSync = require('browser-sync');
 
 gulp.task('less', function () {
-    return gulp.scr('./src/less/main.less')
+    return gulp.src('./src/less/main.less')
         .pipe(less({
             plugins: [autoprefix]
         }))
-        .pipe(gulp.dest('./dist/css'));
-})
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
 
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: './dist'
+    },
+  })
+});
+
+gulp.task('html', function () {
+    return gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', ['browserSync', 'less', 'html'], function() {
+    
+    gulp.watch('./src/less/**/*.less', function() {
+        gulp.run('less');
+    });
+    gulp.watch('./src/**/*.html', function() {
+        gulp.run('html');
+    });
+});
